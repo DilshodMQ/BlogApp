b76e431a5233c8390bfe0d581c03eb5f2c24803f
DilshodMQ <dilshodmahamadiyev1991@gmail.com>
2022-09-29 08:52:33 +0500

    Added Identity

---
+++ MvcApp/MvcApp/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using MvcApp.Areas.Identity.Data;
+
+namespace MvcApp.Models
+{
+    public class Post
+    {
+
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        [ForeignKey("Author")]
+        public string? AuthorId { get; set; }
+        public MvcAppUser? Author { get; set; }
+       
+    }
+}
